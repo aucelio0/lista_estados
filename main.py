cdaf756c4d52d6488d62bb2894179399c52c74b9
aucelio0 <mateusaucelio06@gmail.com>
2024-08-05 17:47:03 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,32 @@
+# lista de dados
+estados = [
+    'Acre',
+    'Amazonas',
+    'Alagoas',
+    'Amapa',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espiríto Santo',
+    'Goías',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul'
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro'
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins'    
+     ]
+for estado in estados:
+    print(estado)
